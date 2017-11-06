b28a19268a392dc045eb41a36c3216490d3a7850
Nazar Ponochevnyi <nazar.ponochevnyi@gmail.com>
2017-11-06 07:39:07 +0200

    Initial commit

--- src/model.py
+++ src/model.py
@@ -1,6 +1,5 @@
 """ Convolutional Neural Network.
 Build and train a convolutional neural network with TensorFlow.
-This example is using the MNIST database of handwritten digits
 (http://yann.lecun.com/exdb/mnist/)
 """
 
